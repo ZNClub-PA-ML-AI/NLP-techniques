e7a8e25093a37573ba595f0e6a52c2713f6a5cf4
ZNevzz <dsouza.nevil45@gmail.com>
2017-02-04 09:55:12 +0530

    new snippets added

---
+++ stopwords.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 09:31:45 2017
+
+@author: ZNevzz
+
+"""
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+example_sent = "This is a sample sentence, showing off the stop words filtration."
+
+stop_words = set(stopwords.words('english'))
+
+word_tokens = word_tokenize(example_sent)
+
+filtered_sentence = [w for w in word_tokens if not w in stop_words]
+
+filtered_sentence = []
+
+for w in word_tokens:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+
+print(word_tokens)
+print(filtered_sentence)
+
+
